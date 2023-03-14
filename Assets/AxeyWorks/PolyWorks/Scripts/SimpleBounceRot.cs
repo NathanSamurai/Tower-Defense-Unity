2cbc219669d13de0757eaed7455dc3bda2df4a71
maxXmeridius <rmalberti00@gmail.com>
2023-03-14 10:48:00 +0100

    Integrazione Package Polyworks e fatto prova in greybox

---
+++ Assets/AxeyWorks/PolyWorks/Scripts/SimpleBounceRot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleBounceRot : MonoBehaviour
+{
+    [Tooltip("Rotation speed.")]
+    public float rotSpeed = 2.0f;
+    public float timeAdjust = 1.0f;
+
+    void Update()
+    {
+        float angle = Mathf.Sin(Time.time * timeAdjust) * (rotSpeed * 10);
+
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+    }
+}
